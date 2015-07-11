40b8b83298f09d08f280e0e52fd4e5b0fe184e20
Kyle Smith <kyle.r.smiff@gmail.com>
2015-07-11 15:44:54 -0700

    Added intro activity animations
    Restyled the intro activty as well

--- java/com/dev/kylesmith/wakeup/controller/IntroController.java
+++ java/com/dev/kylesmith/wakeup/controller/IntroController.java
@@ -1,6 +1,7 @@
 package com.dev.kylesmith.wakeup.controller;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
@@ -15,35 +16,73 @@ import android.widget.LinearLayout;
 import com.dev.kylesmith.wakeup.R;
 import com.dev.kylesmith.wakeup.model.Constants;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by kylesmith on 7/8/15.
  */
 public class IntroController extends Activity {
-    private static final String TAG = "Intro";
-    private static final int SELECT_PICTURE = 1;
-    private ImageView photo;
-    private Uri outputFileUri;
+    @Bind(R.id.signupInstructionsLi1) LinearLayout mInstruction1;
+    @Bind(R.id.signupInstructionsLi2) LinearLayout mInstruction2;
+    @Bind(R.id.signupInstructionsLi3) LinearLayout mInstruction3;
+    @Bind(R.id.buttons_container) LinearLayout mButtonsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         super.onCreate(savedInstanceState);
 
-        SharedPreferences settings = getSharedPreferences(Constants.PREFS_NAME, 0);
-        if (settings.getBoolean(Constants.PREFS_FIRST_TIME, true)) {
-            setContentView(R.layout.activity_signup);
-            settings.edit().putBoolean(Constants.PREFS_FIRST_TIME, false).commit();
-        }
-        else{
+        setLayout();
+        ButterKnife.bind(this);
+
+        startIntroAnimation();
+    }
+
+    private void startIntroAnimation(){
+        int fadeInDuration = 500;
+        int startDelay = 300;
+
+        ValueAnimator fadeIn1 = ObjectAnimator.ofFloat(mInstruction1, "alpha", 0f, 1f);
+        fadeIn1.setDuration(fadeInDuration);
+
+        ValueAnimator fadeIn2 = ObjectAnimator.ofFloat(mInstruction2, "alpha", 0f, 1f);
+        fadeIn2.setDuration(fadeInDuration);
+        fadeIn2.setStartDelay(startDelay);
+
+        ValueAnimator fadeIn3 = ObjectAnimator.ofFloat(mInstruction3, "alpha", 0f, 1f);
+        fadeIn3.setDuration(fadeInDuration);
+        fadeIn3.setStartDelay(startDelay);
+
+        ValueAnimator fadeIn4 = ObjectAnimator.ofFloat(mButtonsContainer, "alpha", 0f, 1f);
+        fadeIn4.setDuration(fadeInDuration);
+        fadeIn4.setStartDelay(startDelay);
+
+        AnimatorSet sequentialFadeIn = new AnimatorSet();
+        sequentialFadeIn.play(fadeIn1).before(fadeIn2);
+        sequentialFadeIn.play(fadeIn2).before(fadeIn3);
+        sequentialFadeIn.play(fadeIn3).before(fadeIn4);
+
+        sequentialFadeIn.setStartDelay(1000);
+
+        sequentialFadeIn.start();
+    }
+
+
+    private void setLayout(){
+        if (false) {
             Intent intent = new Intent(this, ScheduleController.class);
             startActivity(intent);
         }
+        else{
+            setContentView(R.layout.activity_signup);
+        }
+    }
 
-        LinearLayout mAddAClass = (LinearLayout) findViewById(R.id.addAClassli);
-        ValueAnimator fadeIn1 = ObjectAnimator.ofFloat(mAddAClass, "alpha", 0f, 1f);
-        fadeIn1.setDuration(1000);
-        fadeIn1.start();
+
+    private boolean isLoggedIn(){
+        SharedPreferences settings = getSharedPreferences(Constants.PREFS_NAME, 0);
+        return settings.getBoolean(Constants.PREFS_FIRST_TIME, true);
     }
 }
