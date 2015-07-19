1f8ed14e94de6a2f815be59ae254c6fc2ae1bd23
Kyle Smith <kyle.r.smiff@gmail.com>
2015-07-18 23:31:10 -0700

    Added conversion methods to util folder
    Also removed unused Appointment controller

--- java/com/dev/kylesmith/wakeup/model/Appointment/Appointment.java
+++
@@ -1,8 +0,0 @@
-package com.dev.kylesmith.wakeup.model.Appointment;
-
-/**
- * Created by kylesmith on 1/12/15.
- */
-public class Appointment {
-
-}
---
+++ java/com/dev/kylesmith/wakeup/util/Convert.java
@@ -0,0 +1,20 @@
+package com.dev.kylesmith.wakeup.util;
+
+/**
+ * Created by kylesmith on 7/18/15.
+ */
+public class Convert {
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts decimal degrees to radians             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    public static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::  This function converts radians to decimal degrees             :*/
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    public static double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
+    }
+}
